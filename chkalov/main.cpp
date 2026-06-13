68d465b9c747b4b56218ecc1b3d23fa62a92048a
StoYazykov <unix.sto.yazykov@yandex.ru>
2026-06-13 12:34:46 +0300

    Fixed a (build script)

--- chkalov/main.cpp
+++ chkalov/main.cpp
@@ -9,6 +9,13 @@ int main(int argc, char** argv) {
         cout << "Usage: chkalovc input.chk output.cvm (-d)" << endl;
         return 1;
     }
+
+    123.45
+    i=123
+    d=45
+    r=i
+    r+=d/(d/10)
+
     string argi=argv[1], argo=argv[2], argd;
     if(argc==4) argd=argv[3];
     else argd="";
